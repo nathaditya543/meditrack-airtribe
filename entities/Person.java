e2526c3d98af8038a3b3a2bd637cbf740d72f8e1
nathaditya <nathaditya@outlook.com>
2026-02-14 11:34:49 +0530

    initial commit

---
+++ entities/Person.java
@@ -0,0 +1,20 @@
+package entities;
+
+abstract class Person {
+    int id;
+    String name;
+
+    Person(int id, String name){
+        this.id = id;
+        this.name = name;
+    }
+
+    int getId(){
+        return id;
+    }
+
+    String getName(){
+        return name;
+    }
+
+}
